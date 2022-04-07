3890c817204df118f5d43f3b09362e14d8603111
CanezinBeto <albertocanezin@gmail.com>
2022-04-06 21:54:13 -0300

    Começando melhorias no drag in drop

--- javascript/modules/todoList.js
+++ javascript/modules/todoList.js
@@ -88,8 +88,6 @@ export default class ToDoList {
       this.ul.appendChild(this.arrayLi[this.contador])
       this.main.classList.add('list-content')
       this.addValueInputInSpan()
-      this.dragInit()
-      this.dragEnd()
       this.contador++
       this.input.value = ''
     }
@@ -168,57 +166,6 @@ export default class ToDoList {
         if (this.ul.children.length <= 0)
           this.main.classList.remove('list-content')
       })
-    })
-  }
-
-  // Método para evento dragIniti
-  dragInit() {
-    const section = document.querySelector('[data-js="sec-drop"]')
-    const ul = document.querySelector('[data-js="drop"]')
-    const li = document.querySelectorAll('li')
-    const media = matchMedia('(min-width: 650px)').matches
-
-    li.forEach((item) => {
-      item.addEventListener('dragstart', () => {
-        if (media) {
-          section.classList.add('drop-bg')
-          ul.classList.add('drop')
-        }
-      })
-    })
-  }
-
-  // Método para evento de dragEnd
-  dragEnd() {
-    const li = document.querySelectorAll('li')
-
-    li.forEach((item) => {
-      item.addEventListener('dragend', ({ target }) => {
-        const ulDrop = document.querySelector('[data-js="drop"]')
-        const media = matchMedia('(min-width: 650px)').matches
-
-        if (media) ulDrop.appendChild(target)
-        if (this.ul.children.length <= 0)
-          this.main.classList.remove('list-content')
-      })
-    })
-    this.removeAllLiInUlDrop()
-  }
-
-  // Método para remover todas LI
-  removeAllLiInUlDrop() {
-    const li = document.querySelectorAll('li')
-    const ulDrop = document.querySelector('[data-js="drop"]')
-    const btnRemoveDrop = document.querySelector('[data-js="removerDrop"]')
-
-    btnRemoveDrop.addEventListener('click', (event) => {
-      event.preventDefault()
-      if (ulDrop.children.length > 0) {
-        li.forEach((item) => {
-          ulDrop.removeChild(item)
-          this.contador = 0
-        })
-      }
     })
   }
 
